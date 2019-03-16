002300b6f7a2edb933efe0a2f47f3f41166fae22
hermlon <dev.hermlon@yamx.net>
2019-03-16 21:17:41 +0100

    Working with wincompose and without official unicode support

--- keyboards/chibios_test/keymaps/60_EN/keymap.c
+++ keyboards/chibios_test/keymaps/60_EN/keymap.c
@@ -27,7 +27,7 @@ enum custom_keycodes {
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		LAYOUT(
 		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
-                KC_TAB, KC_Q, KC_W, KC_E, KC_AE, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
+                KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
                 KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
                 KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LSFT,
                 KC_LCTL, MO(1), KC_LALT, KC_SPC, KC_ALGR, KC_LGUI, KC_RGUI, MO(2)),
@@ -52,28 +52,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case KC_AE:
       if (record->event.pressed) {
-        SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_KP_1 SS_UP(X_LALT)));
+        SEND_STRING(SS_TAP(X_LGUI) SS_TAP(X_A) SS_LSFT(SS_TAP(X_QUOTE)));
       }
       break;
     case KC_OE:
       if (record->event.pressed) {
-        //SEND_STRING(SS_LALT("148"));
-        SEND_STRING("ö");
+        SEND_STRING(SS_TAP(X_LGUI) SS_TAP(X_O) SS_LSFT(SS_TAP(X_QUOTE)));
       }
       break;
     case KC_UE:
       if (record->event.pressed) {
-        //SEND_STRING(SS_LALT("129"));
-        SEND_STRING("ü");
+        SEND_STRING(SS_TAP(X_LGUI) SS_TAP(X_U) SS_LSFT(SS_TAP(X_QUOTE)));
       }
       break;
     case KC_SS:
       if (record->event.pressed) {
-        //SEND_STRING(SS_LALT("225"));
-        SEND_STRING("ß");
+        SEND_STRING(SS_TAP(X_LGUI) SS_TAP(X_S) SS_TAP(X_S));
       }
       break;
-
 }
   return true;
 };
